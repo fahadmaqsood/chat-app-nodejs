c431c9d1402e8b05921e5121b1e95b37a092f097
fahadmaqsood <qazifahadmaqsood@gmail.com>
2024-09-16 00:58:29 +0500

    fixing socket for chatbot

--- backend/socket/chatbot.socket.js
+++ backend/socket/chatbot.socket.js
@@ -25,9 +25,9 @@ const handleChatbotSocketEvents = (socket, io) => {
                 io.to(socketId).emit('CHAT_MESSAGE_RECEIVED', incomingMessage);
                 io.to(socketId).emit('CHAT_MESSAGE_SENT', outgoingMessage);
 
-                console.log(`data sent to ${userId}: ${subject}`)
+                console.log(`data sent to ${userId}:${subject}`)
             } else {
-                console.error(`No socket found for id: ${userId}: ${subject}`);
+                console.error(`No socket found for id: ${userId}:${subject}`);
             }
         } catch (error) {
             console.error('Error handling chat message:', error.message);
@@ -58,7 +58,8 @@ const initializeChatbotSocket = (io) => {
     io.on('connection', (socket) => {
         console.log('Chatbot socket connected:', socket.id);
 
-        socket.on('REGISTER_USER', (userId, subject) => {
+        socket.on('REGISTER_USER', (data) => {
+            const { userId, subject } = data;
             // Store the mapping of userId to socket.id
             userSocketMap.set(`${userId}:${subject}`, socket.id);
             console.log(`Registered user: ${userId}:${subject} with socket ID: ${socket.id}`);
