967455bff347f47dfd5995f32b936a3a1bfc6204
fahadmaqsood <qazifahadmaqsood@gmail.com>
2024-10-11 05:11:22 +0500

    changing port

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config({
 const majorNodeVersion = +process.env.NODE_VERSION?.split(".")[0] || 0;
 
 const startServer = () => {
-  httpServer.listen(process.env.PORT || 8080, () => {
+  httpServer.listen(process.env.PORT || 3000, () => {
     logger.info("⚙️  Server is running on port: " + process.env.PORT);
   });
 };
