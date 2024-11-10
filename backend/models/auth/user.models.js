26880fb5302c950c7b7a490daf49024f18c3bbff
fahadmaqsood <qazifahadmaqsood@gmail.com>
2024-11-10 22:03:56 +0500

    Email sending

--- backend/models/auth/user.models.js
+++ backend/models/auth/user.models.js
@@ -277,7 +277,7 @@ userSchema.methods.generateTemporaryToken = function () {
 
 userSchema.methods.generateOTP = function () {
   // // Generate a random number between 100000 and 999999
-  const otp = Math.floor(100000 + Math.random() * 900000);
+  const otp = "" + Math.floor(100000 + Math.random() * 900000);
 
   //const otp = "123456";
 
