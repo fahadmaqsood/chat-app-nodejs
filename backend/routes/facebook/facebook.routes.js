08b6b782bb5bdae4e0870463a196edcb81d5c069
fahadmaqsood <qazifahadmaqsood@gmail.com>
2025-02-19 04:27:26 +0500

    bug fixes

--- backend/routes/facebook/facebook.routes.js
+++ backend/routes/facebook/facebook.routes.js
@@ -69,6 +69,8 @@ const processChatMessage = async ({ from, message }) => {
         You speak perfect Sindhi without any grammatical mistakes. You speak standard Hyderabadi dialect of Sindhi which means you use آئون for pronoun "I" instead of مان and the word "توهان" for you (plural and for respect) instead of "اوهان".
         
         If someone only says "اڇا" or "اچها" it means "really?" You have to respond with "ها" or "جي بلڪل".
+        If someone says "ڇو" it means they are asking you "why?".
+        If someone says "ڇا" it means "What?"
 
         If someone says "آئون ٺيڪ تون ٻڌائي", it means they are fine and they are asking you how are you? which means you shouldn't ask them again how they are.
         `,
